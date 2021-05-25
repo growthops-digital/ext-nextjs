01078e7fb8abc4a550ec221053562ee2bdf1d768
Tristan Strathearn <tristan.strathearn@growthops.com.au>
2021-05-25 11:33:16 +1000

    feat: add ProgressBar component

---
+++ src/progress-bar.js
@@ -0,0 +1,38 @@
+import React, {createContext, useContext, useMemo} from 'react';
+import PropTypes from 'prop-types';
+import Router from 'next/router.js';
+import NProgress from 'nprogress';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+const Context = createContext();
+
+const useProgressBar = () => useContext(Context);
+
+const enableProgressBar = () => NProgress.start();
+const disableProgressBar = () => NProgress.done();
+
+const ProgressBar = ({children}) => {
+	const contextData = useMemo(() => ({
+		enableProgressBar,
+		disableProgressBar
+	}), []);
+
+	return (
+		<Context.Provider value={contextData}>
+			{children}
+		</Context.Provider>
+	);
+};
+
+ProgressBar.propTypes = {
+	children: PropTypes.element
+};
+
+export {
+	useProgressBar
+};
+
+export default ProgressBar;
