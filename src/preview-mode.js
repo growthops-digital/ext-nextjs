6aa391c2a8ff459b538aa95f2e2393cac4154098
Tristan Strathearn <tristan.strathearn@growthops.com.au>
2021-05-31 14:44:17 +1000

    fix(PreviewMode): avoid newlines and tabs in final transpiled string

--- src/preview-mode.js
+++ src/preview-mode.js
@@ -2,43 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {useRouter} from 'next/router.js';
 
-const rootClasses = `
-	bg-yellow-200
-	border-b-4
-	border-yellow-600
-	bottom-0
-	fixed
-	flex
-	items-center
-	mr-4
-	pb-3
-	pt-4
-	px-6
-	right-0
-	rounded-t-lg
-	shadow-xl
-	space-x-3
-	text-yellow-900
-	z-50
-`;
-
-const labelClasses = `
-	font-bold
-	text-sm
-	uppercase
-`;
-
-const linkClasses = `
-	bg-yellow-400
-	border-l
-	border-yellow-300
-	font-medium
-	px-2
-	py-1
-	rounded-full
-	text-xs
-	uppercase
-`;
+const rootClasses = 'bg-yellow-200 border-b-4 border-yellow-600 bottom-0 fixed flex items-center mr-4 pb-3 pt-4 px-6 right-0 rounded-t-lg shadow-xl space-x-3 text-yellow-900 z-50';
+const labelClasses = 'font-bold text-sm uppercase';
+const linkClasses = 'bg-yellow-400 border-l border-yellow-300 font-medium px-2 py-1 rounded-full text-xs uppercase';
 
 const PreviewMode = ({disableUrl}) => {
 	const router = useRouter();
